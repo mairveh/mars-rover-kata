f936568e648f7222b71e42d1547744dbf6d97e82
Merve <babutine@yahoo.com>
2023-11-08 14:42:06 +0000

    test: add navigation test cases for happy path

--- tests/rover.test.ts
+++ tests/rover.test.ts
@@ -14,5 +14,20 @@ describe("test Mars Rover inputs with failing edge scenarios", () => {
 });
 
 describe("test Mars Rover inputs in a 5x5 grid", () => {
-
+  it("test for Rover 1 on 5x5 grid", () => {
+    const input = "5 5\n1 2 N\nLMLMLMLMM";
+    const expectedResult = "1 3 N";
+    //Act
+    const result = executeCommands(input);
+    //Assert
+    expect(result).toBe(expectedResult);
+  });
+  it("test for Rover 2 on 5x5 grid", () => {
+    const input = "3 3 E\nMMRMMRMRRM";
+    const expectedResult = "5 1 E";
+    //Act
+    const result = executeCommands(input);
+    //Assert
+    expect(result).toBe(expectedResult);
+  });
 });
